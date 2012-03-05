c1a6e3b1eb51494adf8555de0f18d802dd5bbd56
Orestis Akribopoulos <akribopo@cti.gr>
2012-03-05 14:44:27 +0200

    Working on AOP & Cachable annotation library.

---
+++ Cache/src/main/java/eu/uberdust/caching/CachingAspect.java
@@ -0,0 +1,98 @@
+package eu.uberdust.caching;
+
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Element;
+import org.apache.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+
+@Aspect
+public class CachingAspect {
+
+    private static final Logger LOGGER = Logger.getLogger(CachingAspect.class);
+
+    private final CacheManager singletonManager;
+
+    public CachingAspect() {
+        singletonManager = CacheManager.create();
+
+    }
+
+    /**
+     * This pointcut matches all methods with a <code>@Cachable</code> annotation.
+     */
+    @Pointcut("execution(@Cachable * *.*(..))")
+    @SuppressWarnings("unused")
+    private void cache() {
+    }
+
+    /**
+     * Returns objects from the cache if necessary.
+     */
+    @Around("cache()")
+    public Object aroundCache(final ProceedingJoinPoint thisJoinPoint) throws Throwable {
+        final String thisJoinPointName = getJoinPointName(thisJoinPoint);
+        final String thisJoinPointArgs = getJointPointArgs(thisJoinPoint);
+        final String objName = thisJoinPointName + "-" + thisJoinPointArgs;
+        LOGGER.info("Injecting method: " + objName);
+
+
+
+        if (singletonManager.cacheExists(thisJoinPointName)) {
+            final Cache cache = singletonManager.getCache(thisJoinPointName);
+
+            if (cache.getKeysWithExpiryCheck().contains(thisJoinPoint.getArgs()[0].hashCode())) {
+                LOGGER.info(" object was on: " + cache.getName());
+                return cache.get(thisJoinPoint.getArgs()[0].hashCode()).getValue();
+            } else {
+                final Element element = new Element(thisJoinPoint.getArgs()[0].hashCode(), thisJoinPoint.proceed());
+                cache.put(element);
+                LOGGER.info(" new object on: " + cache.getName());
+                return element.getValue();
+            }
+        } else {
+            singletonManager.addCacheIfAbsent("defaultCache");
+
+            final Cache cache = singletonManager.getCache("defaultCache");
+
+            if (cache.getKeysWithExpiryCheck().contains((thisJoinPointName + thisJoinPoint.getArgs()[0]).hashCode())) {
+
+                LOGGER.info(" object was on: " + cache.getName());
+
+                return cache.get((thisJoinPointName + thisJoinPoint.getArgs()[0]).hashCode()).getValue();
+            } else {
+                final Element element = new Element((thisJoinPointName + thisJoinPoint.getArgs()[0]).hashCode(),
+                        thisJoinPoint.proceed());
+                LOGGER.info(" new object on: " + cache.getName());
+                cache.put(element);
+                return element.getValue();
+            }
+        }
+    }
+
+    /**
+     * Convenience method that returns the class- and method-name for a given join point.
+     */
+    public final String getJoinPointName(final JoinPoint joinPoint) {
+        return joinPoint.getThis().getClass().getCanonicalName()+ "." + joinPoint.getSignature().getName();
+    }
+
+    /**
+     * Returns the arguments of the current join point as a string.
+     *
+     * @param joinPoint
+     * @return string representing the arguments of this join point
+     */
+    public final String getJointPointArgs(final JoinPoint joinPoint) {
+        final StringBuilder buf = new StringBuilder();
+        for (final Object arg : joinPoint.getArgs()) {
+            buf.append(arg.getClass().getSimpleName()).append("-").append(arg.hashCode());
+        }
+        return buf.toString().replaceAll("\\+$", "");
+    }
+}
---
+++ Cache/src/test/java/eu/uberdust/caching/CacheTest.java
@@ -0,0 +1,33 @@
+package eu.uberdust.caching;
+
+
+import org.apache.log4j.Logger;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: akribopo
+ * Date: 3/5/12
+ * Time: 1:22 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CacheTest {
+
+    private final static Logger LOGGER = Logger.getLogger(CacheTest.class);
+
+    @Cachable
+    public final int cacheTest(final int input) {
+        LOGGER.info("Executing function cacheTest");
+        return input;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        CacheTest cacheTest = new CacheTest();
+        for (int j = 0; j < 20; j++) {
+            for (int i = 0; i < 10; i++) {
+                cacheTest.cacheTest(i);
+            }
+            Thread.sleep(10000);
+        }
+
+    }
+}
