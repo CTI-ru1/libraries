a18303c2ef81e5b36de46106444b94e3c9078d6e
Orestis Akribopoulos <akribopo@cti.gr>
2012-03-20 17:43:34 +0200

    Working on WebSockets

---
+++ WebSocketsLibrary/src/main/java/eu/uberdust/communication/websocket/WSIdentifiers.java
@@ -0,0 +1,42 @@
+package eu.uberdust.communication.websocket;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: akribopo
+ * Date: 3/20/12
+ * Time: 5:28 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface WSIdentifiers {
+
+    /**
+     * Websocket url prefix.
+     */
+    public static final String WS_PREFIX = "ws://";
+
+    /**
+     * Http url prefix.
+     */
+    public static final String HTTP_PREFIX = "http://";
+
+    /**
+     * Insert protocol.
+     */
+    public static final String INSERT_PROTOCOL = "INSERTREADING";
+
+    /**
+     * Testbed Controller.
+     */
+    public static final String COMMAND_PROTOCOL = "TESTBEDCONTROLLER";
+
+    /**
+     * Subscribe protocol prefix.
+     */
+    public static final String SUBSCRIBE_PROTOCOL_PREFIX = "SUB";
+
+    /**
+     * Listener Protocol delimiter.
+     */
+    public static final String DELIMITER = "@";
+
+}
