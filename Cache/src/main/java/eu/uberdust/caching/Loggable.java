5a468d5450162d500b621616e7adc3347c0e285b
Dimitrios Amaxilatis <amaxilat@cti.gr>
2012-05-25 14:54:57 +0300

    loggable annotation

---
+++ Cache/src/main/java/eu/uberdust/caching/Loggable.java
@@ -0,0 +1,18 @@
+package eu.uberdust.caching;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: akribopo
+ * Date: 3/3/12
+ * Time: 12:24 AM
+ * To change this template use File | Settings | File Templates.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Loggable {
+}
