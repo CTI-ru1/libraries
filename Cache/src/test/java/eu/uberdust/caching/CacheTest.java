4b4c3d6310a4cbf99740921a248864a9296416de
Dimitrios Amaxilatis <amaxilat@cti.gr>
2013-07-17 12:19:32 +0300

    add test for cache operations

--- Cache/src/test/java/eu/uberdust/caching/CacheTest.java
+++ Cache/src/test/java/eu/uberdust/caching/CacheTest.java
@@ -2,6 +2,9 @@ package eu.uberdust.caching;
 
 
 import org.apache.log4j.Logger;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by IntelliJ IDEA.
@@ -14,26 +17,66 @@ public class CacheTest {
 
     private final static Logger LOGGER = Logger.getLogger(CacheTest.class);
 
+    static int times_in_someMethod = 0;
+    static int times_in_cacheTest = 0;
+    static int times_in_addNodeReading = 0;
+
+
+    @Cachable
+    int someMethod(int i) {
+        times_in_someMethod++;
+        return times_in_someMethod;
+    }
+
     @Cachable
     public final int cacheTest(final int input) {
         LOGGER.info("Executing function cacheTest");
+        times_in_cacheTest++;
         return input;
     }
 
-    public static void main(String[] args) throws InterruptedException {
+    @EvictCache(cacheName = "")
+    public void addNodeReading(int i) {
+        times_in_addNodeReading++;
+        LOGGER.info("adding :" + i);
+    }
+
+    @Test
+    public void testCacheable() throws InterruptedException {
+        LOGGER.info("testCacheable");
         CacheTest cacheTest = new CacheTest();
         for (int j = 0; j < 20; j++) {
-            for (int i = 0; i < 10; i++) {
-                  cacheTest.addNodeReading(i);
-                //cacheTest.cacheTest(i);
-            }
-            Thread.sleep(1000);
-            //cacheTest.addNodeReading(j);
+            cacheTest.someMethod(1);
         }
+        assertEquals(1, times_in_someMethod);
+    }
 
+    @Test
+    public void testCacheOperations() throws InterruptedException {
+        LOGGER.info("testCacheOperations");
+
+        CacheTest cacheTest = new CacheTest();
+        for (int j = 0; j < 20; j++) {
+            cacheTest.addNodeReading(j);
+        }
+        assertEquals(20, times_in_addNodeReading);
+        LOGGER.info("added Values");
+        for (int j = 0; j < 20; j++) {
+            for (int i = 0; i < 20; i++) {
+                cacheTest.cacheTest(i);
+            }
+        }
+        assertEquals(20, times_in_cacheTest);
+        LOGGER.info("got Cache Hits");
+        cacheTest.addNodeReading(99);
+        LOGGER.info("evicted Caches");
+        for (int j = 0; j < 20; j++) {
+            for (int i = 0; i < 20; i++) {
+                cacheTest.cacheTest(i);
+            }
+        }
+        assertEquals(40, times_in_cacheTest);
+        LOGGER.info("got Empty Cache");
     }
-    @EvictCache(cacheName = "oeo")
-    public void addNodeReading(int i ){
-        System.out.println(i);
-    }
+
 }
